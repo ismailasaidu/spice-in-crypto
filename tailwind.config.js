15a4e3f21a665eb4004cf094f0ad50088364c632
Ismaila Saidu <ismailasaidu997@gmail.com>
2023-09-02 19:01:20 +0100

    omo

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@
 module.exports = {
  
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
-  theme: {
+ 
     extend: {},
     theme: {
       extend: {
@@ -54,7 +54,6 @@ module.exports = {
       },
     },
     plugins: [],
-  },
-  plugins: [],
+ 
 }
 
