7cccf3a36f9e15998bebbbd9be80abff4c3db02a
Ismaila Saidu <ismailasaidu997@gmail.com>
2023-09-02 19:28:34 +0100

    hmm

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,82 @@
+// /** @type {import('tailwindcss').Config} */
+// module.exports = {
+//   plugins:[],
+//   content: ["./src/**/*.{js,jsx,ts,tsx}"],
+
+//   theme: {
+
+//     extend: {},
+
+//     flexGrow: {
+//       2: "2",
+//     },
+
+//     values: {
+//       "100px": "100px",
+//     },
+
+//     colors: {
+//       blue: "#0F1231",
+//       black: "#000000",
+//       headerwhite: "#EFEEEF",
+//       lightblack: "#191919",
+//       textcolor: "#272A31",
+//       lightblue: "#2C9ED7",
+//       white: "#FFFFFF",
+//       footer: "#8B8A8B",
+//       grey: "#716B6B",
+//       rescol: "#1d1d1d",
+//       divider: "#E8E8E8",
+//       darktext: "#3A3939",
+//       back: "#EBEBEB",
+//       anime: "#74DAB5",
+//     },
+//     fontFamily: {
+//       MT: ["Montserrat'"],
+//     },
+//     screens: {
+//       "2xl": { max: "1535px" },
+//       // => @media (max-width: 1535px) { ... }
+
+//       xl: { max: "1279px" },
+//       // => @media (max-width: 1279px) { ... }
+
+//       lg: { max: "1023px" },
+//       // => @media (max-width: 1023px) { ... }
+
+//       md: { max: "1224px" },
+//       // => @media (max-width: 767px) { ... }
+
+//       sm: { max: "780px" },
+//       // => @media (max-width: 639px) { ... }
+
+//       // 'sm-landscape': {'raw': '(max-width: 639px) and (orientation: landscape)'},
+//     },
+//   },
+// };
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  plugins:[],
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
-
   theme: {
-   
     extend: {},
+    screens: {
+      "2xl": { max: "1535px" },
+      // => @media (max-width: 1535px) { ... }
 
-    flexGrow: {
-      2: "2",
-    }, 
+      xl: { max: "1279px" },
+      // => @media (max-width: 1279px) { ... }
 
-    values: {
-      "100px": "100px",
+      lg: { max: "1023px" },
+      // => @media (max-width: 1023px) { ... }
+
+      md: { max: "1224px" },
+      // => @media (max-width: 767px) { ... }
+
+      sm: { max: "780px" },
+      // => @media (max-width: 639px) { ... }
+
+      // 'sm-landscape': {'raw': '(max-width: 639px) and (orientation: landscape)'},
     },
-
     colors: {
       blue: "#0F1231",
       black: "#000000",
@@ -34,23 +96,6 @@ module.exports = {
     fontFamily: {
       MT: ["Montserrat'"],
     },
-    screens: {
-      "2xl": { max: "1535px" },
-      // => @media (max-width: 1535px) { ... }
-
-      xl: { max: "1279px" },
-      // => @media (max-width: 1279px) { ... }
-
-      lg: { max: "1023px" },
-      // => @media (max-width: 1023px) { ... }
-
-      md: { max: "1224px" },
-      // => @media (max-width: 767px) { ... }
-
-      sm: { max: "780px" },
-      // => @media (max-width: 639px) { ... }
-
-      // 'sm-landscape': {'raw': '(max-width: 639px) and (orientation: landscape)'},
-    },
   },
+  plugins: [],
 };
