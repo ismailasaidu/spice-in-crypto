f5324c6ff0a93f55e528a726deb5c018557420a1
Ismaila Saidu <ismailasaidu997@gmail.com>
2023-11-20 00:22:17 +0100

    new commit

--- src/Component/Hero.jsx
+++ src/Component/Hero.jsx
@@ -18,16 +18,17 @@ const Hero = () => {
     <div>
       <div className="bg-[url('./Assets/peakpx.jpg')]  bg-cover bg-center relative  bg-no-repeat h-[1300px] sm:h-[100%] md:h-[800px] py-[80px] sm:px-[30px] sm:pt-[80px] pt-[150px] md:pt-[120px] px-[150px] ">
         <Link to="/signals">
-      <div className="text-center mt-[-80px] sm:mt-[60px]">
-        <button className="bg-lightblue w-[100px] h-[40px] text-[16px] font-bold text-white rounded-md">Join Us</button>
-      </div>
-      </Link>
+          <div className="text-center mt-[-40px] sm:mt-[60px]">
+            <button className="bg-lightblue w-[100px] h-[40px] text-[16px] font-bold text-white rounded-md">
+              Join Us
+            </button>
+          </div>
+        </Link>
         <div className="flex flex-row md:flex-col justify-between mt-[40px] sm:mt-0 absolute">
           <div data-aos="fade-right">
-            
             <div className="sm:hidden ">
               <h1 className=" content  text-[34px]  ml-[50px] md:block sm:ml-0 sm:text-[54px] md:ml-0 md:text-[34px]  md:text-left   font-black">
-              Trade Smarter, Not Harder!
+                Trade Smarter, Not Harder!
               </h1>
             </div>
             <div className="hidden sm:block ">
@@ -37,18 +38,24 @@ const Hero = () => {
             </div>
             <div className="mt-[20px] sm:mt-[30px]    sm:text-[16px] sm:flex sm:flex-col sm:gap-[5px] gap-[15px]">
               <div className="font-bold text-textcolor flex flex-row items-center mt-[30px] sm:mt-0">
-                <p className="font-bold text-black sm:hidden block ">Your Journey to Financial Freedom Begins Here!</p>
-                <p className="font-bold text-black sm:block hidden">Your Journey to Financial Freedom Begins <br></br>Here!</p>
+                <p className="font-bold text-black sm:hidden block ">
+                  Your Journey to Financial Freedom Begins Here!
+                </p>
+                <p className="font-bold text-black sm:block hidden">
+                  Your Journey to Financial Freedom Begins <br></br>Here!
+                </p>
               </div>
 
               <div className="font-bold flex flex-row items-start ">
                 {/* <p>.</p> */}
-                <p>Knowledge gathered within 7 years <br></br>of trading broken down<br></br> for easy assimilation. </p>
-               
+                <p>
+                  Knowledge gathered within 7 years <br></br>of trading broken
+                  down<br></br> for easy assimilation.{" "}
+                </p>
               </div>
               <div className="font-bold flex flex-row items-start ">
-              {/* <p>.</p> */}
-              <p>Let's get you started on a life changing journey.</p>
+                {/* <p>.</p> */}
+                <p>Let's get you started on a life changing journey.</p>
               </div>
               {/* <div className="font-bold text-black flex flex-row items-center ">
                 <p>.</p>
@@ -87,15 +94,14 @@ const Hero = () => {
             <iframe
               className="w-[1000px] sm:w-[100%] md:w-[100%] sm:h-[200px]  md:h-[250px]   h-[500px]"
               // src="https://www.youtube.com/embed/_F8MgJ3FVm8"
-              src="https://www.youtube.com/embed/nK8fzdffwYw?si=I0N4cx6eNGA6XYc7" 
+              src="https://www.youtube.com/embed/nK8fzdffwYw?si=I0N4cx6eNGA6XYc7"
               title="YouTube video"
               allowFullScreen></iframe>
           </div>
         </Container>
         <div className="mt-[50px] ">
-        <Started  text1="ENROLL NOW" />
+          <Started text1="ENROLL NOW" />
         </div>
-   
       </div>
       {/* <Route path="description/:ids" element={<ProductDetails/>}/> */}
     </div>
