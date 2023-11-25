e684bc4a4692db0c3fd01fc5d00e49587bfa516c
Ismaila Saidu <ismailasaidu997@gmail.com>
2023-11-25 23:23:01 +0100

    Clear Git cache

--- src/redux/Store.js
+++ src/redux/Store.js
@@ -2,15 +2,9 @@ import { configureStore } from "@reduxjs/toolkit";
 import CartSlice from "./CartSlice";
 import AuthSlice from "./AuthSlice";
 
-
 export const store = configureStore({
   reducer: {
     cart: CartSlice,
-    auth: AuthSlice
+    auth: AuthSlice,
   },
 });
-
-
-  
-  
-  
